c960a979edac555508f9f10b9f8ae4a2e1632728
Sander van den Hoed <s.vdhoed@gmail.com>
2019-06-13 17:19:43 +0200

    Add project files.

---
+++ LolSharpLoader/Settings.cs
@@ -0,0 +1,16 @@
+﻿namespace LolSharpLoader
+{
+    public class Settings
+    {
+        public string BaseUrl { get; set; }
+        public string ListFileName { get; set; }
+        public string UrlPath { get; set; }
+        public ClientType ClientType { get; set; }
+    }
+
+    public enum ClientType
+    {
+        Mac,
+        Windows
+    }
+}
